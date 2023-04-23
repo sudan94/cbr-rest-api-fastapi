03d0cc5262fc63ea6197e3eb7347c810daf58bab
sudan94 <sudanupadhaya@gmail.com>
2023-04-23 14:51:21 +0200

    included other features and added distance calculation for float

--- similarity.py
+++ similarity.py
@@ -5,21 +5,17 @@ def find_similar_cases_by_distance(new_problem, case_library, k=4, distance_metr
     # define distance function for numerical features
     def numerical_distance(case1, case2):
         # select numerical features to normalize
-        numerical_features = ['problem_population', 'problem_start_number_of_active_cases', 'problem_end_number_of_active_cases', 'problem_start_number_of_icu_active_cases',
-                              'problem_end_number_of_icu_active_cases', 'problem_start_number_of_deaths', 'problem_end_number_of_deaths', 'problem_vaccinated_population']
+        numerical_features = ['problem_population', 'problem_age_distribution', 'problem_start_number_of_active_cases', 'problem_end_number_of_active_cases', 'problem_start_number_of_icu_active_cases', 'problem_end_number_of_icu_active_cases', 'problem_start_number_of_deaths', 'problem_end_number_of_deaths', 'problem_vaccinated_population','problem_average_temprature','problem_average_humidity','problem_mortality_rate','problem_infection_rate']
 
-        # extract numerical features from inputs
-        case1_num = np.array([case1[f] for f in numerical_features])
-        case2_num = np.array([case2[f] for f in numerical_features])
-
-        # calculate z-score normalization for both vectors
+         # extract numerical features from inputs
+        case1_num = np.array([case1[f] for f in numerical_features], dtype=float)
+        case2_num = np.array([case2[f] for f in numerical_features], dtype=float)
+                # calculate z-score normalization for both vectors
         case1_norm = (case1_num - np.mean(case1_num)) / np.std(case1_num)
         case2_norm = (case2_num - np.mean(case2_num)) / np.std(case2_num)
-
-        # calculate Euclidean distance between the normalized vectors
+                # calculate Euclidean distance between the normalized vectors
         distance = np.linalg.norm(case1_norm - case2_norm)
-
-        # return distance
+                # return distance
         return distance
 
     # compute distance between new problem and each case in the case library
